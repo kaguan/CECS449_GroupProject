338ca18ea6d8c45a1993ea1b60005f72c94133c2
kaguan <karen.guan@student.csulb.edu>
2023-06-30 19:42:11 -0700

    fixing the textures

--- textures.py
+++ textures.py
@@ -1,4 +1,4 @@
-import pygame as pg
+import pygame
 import moderngl as mgl
 
 
@@ -9,31 +9,35 @@ class Texture:
         self.textures['skybox'] = self.get_texture_cube(dir_path='textures/', ext='jpg')
 
     def get_texture_cube(self, dir_path, ext='jpg'):
-        faces = ['right', 'left', 'up', 'down'] + ['front', 'back'][::-1]
+        faces = ['right', 'left', 'top', 'bottom'] + ['front', 'back'][::-1]
         # textures = [pg.image.load(dir_path + f'{face}.{ext}').convert() for face in faces]
         textures = []
         for face in faces:
-            texture = pg.image.load(dir_path + f'{face}.{ext}').convert()
+            texture = pygame.image.load(dir_path + f'{face}.{ext}').convert()
             if face in ['right', 'left', 'front', 'back']:
-                texture = pg.transform.flip(texture, flip_x=True, flip_y=False)
+                texture = pygame.transform.flip(texture, flip_x=True, flip_y=False)
+            elif face in ['top']:
+                texture = pygame.transform.flip(texture, flip_x=False, flip_y=True)
+                texture = pygame.transform.rotate(texture, 90)
             else:
-                texture = pg.transform.flip(texture, flip_x=False, flip_y=True)
+                texture = pygame.transform.flip(texture, flip_x=True, flip_y=False)
+                texture = pygame.transform.rotate(texture, 90)
             textures.append(texture)
 
         size = textures[0].get_size()
         texture_cube = self.ctx.texture_cube(size=size, components=3, data=None)
 
         for i in range(6):
-            texture_data = pg.image.tostring(textures[i], 'RGB')
+            texture_data = pygame.image.tostring(textures[i], 'RGB')
             texture_cube.write(face=i, data=texture_data)
 
         return texture_cube
 
     def get_texture(self, path):
-        texture = pg.image.load(path).convert()
-        texture = pg.transform.flip(texture, flip_x=False, flip_y=True)
+        texture = pygame.image.load(path).convert()
+        texture = pygame.transform.flip(texture, flip_x=False, flip_y=True)
         texture = self.ctx.texture(size=texture.get_size(), components=3,
-                                   data=pg.image.tostring(texture, 'RGB'))
+                                   data=pygame.image.tostring(texture, 'RGB'))
         # mipmaps
         texture.filter = (mgl.LINEAR_MIPMAP_LINEAR, mgl.LINEAR)
         texture.build_mipmaps()
